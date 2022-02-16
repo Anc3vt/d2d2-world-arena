25c8fb856f12046e35a26c603a810f575eab5467
Ancevt <i@ancevt.ru>
2022-02-16 20:19:14 +0300

    2022-02-16_20-19-14

--- git-clone-modules.sh
+++ git-clone-modules.sh
@@ -8,5 +8,5 @@ git clone https://github.com/Anc3vt/d2d2-world-client
 git clone https://github.com/Anc3vt/d2d2-world-desktop
 git clone https://github.com/Anc3vt/d2d2-world-server
 git clone https://github.com/Anc3vt/d2d2-network-messaging
-git clone https://github.com/Anc3vt/d2d2-ttf3bmf
+git clone https://github.com/Anc3vt/d2d2-ttf2bmf
 
