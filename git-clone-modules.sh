d73a291449ea10b7020cd7da12231e9d75c5c371
Ancevt <i@ancevt.ru>
2022-02-16 20:17:01 +0300

    Add git-clone-modules.sh

---
+++ git-clone-modules.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://github.com/Anc3vt/d2d2-core
+git clone https://github.com/Anc3vt/d2d2-panels
+git clone https://github.com/Anc3vt/d2d2-world
+git clone https://github.com/Anc3vt/d2d2-world-editor
+git clone https://github.com/Anc3vt/d2d2-world-client
+git clone https://github.com/Anc3vt/d2d2-world-desktop
+git clone https://github.com/Anc3vt/d2d2-world-server
+git clone https://github.com/Anc3vt/d2d2-network-messaging
+git clone https://github.com/Anc3vt/d2d2-ttf3bmf
+
