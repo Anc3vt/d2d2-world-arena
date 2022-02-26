a8a13e3f093fa16dde66df647140e7dfc356fb66
Ancevt <i@ancevt.ru>
2022-02-26 12:31:53 +0300

    Add common d2d2-world-common-tileset.png

--- d2d2-world/src/main/java/ru/ancevt/d2d2world/ui/Preloader.java
+++ d2d2-world/src/main/java/ru/ancevt/d2d2world/ui/Preloader.java
@@ -2,18 +2,22 @@ package ru.ancevt.d2d2world.ui;
 
 import ru.ancevt.d2d2.D2D2;
 import ru.ancevt.d2d2.display.DisplayObjectContainer;
+import ru.ancevt.d2d2.display.Root;
 import ru.ancevt.d2d2.display.Sprite;
+import ru.ancevt.d2d2.display.texture.Texture;
+import ru.ancevt.d2d2.lwjgl.LWJGLStarter;
 
 public class Preloader extends DisplayObjectContainer {
-    public static final String ASSET = "d2d2-world-loading-image.png";
+
+    public static final String ASSET = "d2d2-world-common-tileset.png";
 
     private static final int FRAMES = 10;
     private int timer = FRAMES;
 
-    private static Sprite sprite;
+    private static Texture texture;
 
     public Preloader() {
-        add(getSprite(), -32, -32);
+        add(new Sprite(getOrLoadTexture()), -32, -32);
     }
 
     @Override
@@ -24,10 +28,16 @@ public class Preloader extends DisplayObjectContainer {
         }
     }
 
-    private static Sprite getSprite() {
-        if(sprite == null) {
-            sprite = new Sprite(D2D2.getTextureManager().loadTextureAtlas(ASSET).createTexture());
+    private static Texture getOrLoadTexture() {
+        if (texture == null) {
+            texture = D2D2.getTextureManager().loadTextureAtlas(ASSET).createTexture(32, 0, 64, 64);
         }
-        return sprite;
+        return texture;
+    }
+
+    public static void main(String[] args) {
+        Root root = D2D2.init(new LWJGLStarter(800, 600, "(floating)"));
+        root.add(new Preloader(), 32, 32);
+        D2D2.loop();
     }
 }
