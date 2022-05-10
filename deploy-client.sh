49ac53fbfcce75f48aea8d5ed73a8d0e6dd23315
Ancevt <i@ancevt.ru>
2022-05-10 22:52:10 +0300

    add sh scripts: deploy-server.sh, deploy-client.sh

---
+++ deploy-client.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+./clean.sh
+
+while getopts "adh" opt
+do
+   	case $opt in
+        w) BUILD_WEB_TARGET=$OPTARG;;
+	    d) DEFAULT_GAME_SERVER=$OPTARG;;
+        h) printf "Example:\n./deploy-client.sh -w 'd2d2.world:/var/www/d2d2.world/builds' -d 'd2d2.world:3333'\n"
+            exit;;
+    	*) ;;
+	esac
+done
+
+if [[ -z $BUILD_WEB_TARGET ]]; then
+    BUILD_WEB_TARGET='ancevt@d2d2.world:/var/www/d2d2.world/builds/'
+fi
+
+if [[ -z $DEFAULT_GAME_SERVER ]]; then
+    DEFAULT_GAME_SERVER="d2d2.world:3333"
+fi
+
+echo Start client deploying...
+echo Build web target: $BUILD_WEB_TARGET
+echo Client default game server is $DEFAULT_GAME_SERVER
+
+sleep 3
+
+mvn clean install -Ddefault-game-server=$DEFAULT_GAME_SERVER -Pdesktop -Pexe \
+|| {
+    printf "\nAborted"
+    exit;
+}
+
+mkdir -p buildtmp/
+cd buildtmp/
+cp -v ../d2d2-world-arena-desktop/target/*dep* .
+cp -v ../d2d2-world-arena-desktop/target/*exe .
+
+rename 's/-jar-with-dependencies//' *.jar
+
+scp * "$BUILD_WEB_TARGET"
+
+echo Complete
+
---
+++ deploy-server.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+./clean.sh
+
+while getopts "awh" opt
+do
+    case $opt in
+        a) HOST=$OPTARG;;
+        w) BUILD_WEB_TARGET=$OPTARG;;
+        h) printf "Example:\n./deploy-server.sh -a 'd2d2.world' -w 'd2d2.world:/var/www/d2d2.world/builds'\n"
+            exit;;
+        *) ;;
+    esac
+done
+
+if [[ -z $HOST ]]; then
+    HOST='ancevt@d2d2.world'
+fi
+
+if [[ -z $BUILD_WEB_TARGET ]]; then
+    BUILD_WEB_TARGET="d2d2.world:/var/www/d2d2.world/builds/"
+fi
+
+echo Start server deploying...
+echo Host: $HOST
+echo Build web target: $BUILD_WEB_TARGET
+sleep 3
+
+mvn clean install -Pserver \
+|| {
+    printf "\nAborted"
+    exit;
+}
+
+mkdir -p buildtmp/
+cd buildtmp/
+cp -v ../d2d2-world-arena-server/target/*dep* .
+
+rename 's/-jar-with-dependencies//' *.jar
+
+scp * "$BUILD_WEB_TARGET"
+scp * "$HOST:app"
+
+cd ..
+ssh $HOST "rm -rf app/data/"
+scp -r "d2d2-world-arena-server/data" "$HOST:app/"
+ssh $HOST "ps x | grep java | grep 'd2d2-world-arena' | sed 's/^ *//i' | cut -d ' ' -f 1 | xargs kill -9"
+
+echo Server deployment complete
+
