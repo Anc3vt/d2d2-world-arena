5ea7a415f6082f963cd8d17bdaaa1521629e5989
Ancevt <i@ancevt.ru>
2022-05-23 16:21:03 +0300

    DebugPanel: store data in localstorage

--- d2d2-core/src/main/java/com/ancevt/d2d2/debug/DebugPanel.java
+++ d2d2-core/src/main/java/com/ancevt/d2d2/debug/DebugPanel.java
@@ -1,6 +1,7 @@
 
 package com.ancevt.d2d2.debug;
 
+import com.ancevt.commons.util.ApplicationMainClassNameExtractor;
 import com.ancevt.d2d2.D2D2;
 import com.ancevt.d2d2.backend.lwjgl.LWJGLBackend;
 import com.ancevt.d2d2.common.PlainRect;
@@ -13,26 +14,24 @@ import com.ancevt.d2d2.event.InputEvent;
 import com.ancevt.d2d2.event.TouchButtonEvent;
 import com.ancevt.d2d2.input.MouseButton;
 import com.ancevt.d2d2.touch.TouchButton;
+import com.ancevt.localstorage.FileLocalStorage;
+import com.ancevt.localstorage.LocalStorage;
+import com.ancevt.localstorage.LocalStorageBuilder;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Supplier;
 
 import static com.ancevt.d2d2.input.KeyCode.isShift;
-import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
-import static java.nio.file.StandardOpenOption.WRITE;
 
 public class DebugPanel extends DisplayObjectContainer {
 
     private static final Map<String, DebugPanel> debugPanels = new HashMap<>();
     private static boolean enabled;
     private static float scale = 1;
+    private static LocalStorage localStorage;
 
     private final BitmapText text;
     private final String systemPropertyName;
@@ -159,36 +158,42 @@ public class DebugPanel extends DisplayObjectContainer {
         if (text.getBoundWidth() < 10) text.setBoundWidth(10);
     }
 
-    private void save() {
-        try {
-            Files.writeString(Path.of(systemPropertyName + ".tmp"),
-                    (int) getX() + ";" + (int) getY() + ";" + (int) bg.getWidth() + ";" + (int) bg.getHeight() + ";" +
-                            text.getText(), WRITE, CREATE, TRUNCATE_EXISTING);
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static LocalStorage getLocalStorage() {
+        if (localStorage == null) {
+            localStorage = new LocalStorageBuilder("debug-panel.ls", FileLocalStorage.class)
+                    .storageId("d2d2-debug-panel-" + ApplicationMainClassNameExtractor.get())
+                    .saveOnWrite(true)
+                    .build();
         }
+        return localStorage;
+    }
+
+    private void save() {
+        getLocalStorage().put(systemPropertyName, (int) getX() + ";" +
+                (int) getY() + ";" +
+                (int) bg.getWidth() + ";" +
+                (int) bg.getHeight() + ";" +
+                text.getText().replace("\n", "\\n")
+        );
     }
 
     private void load() {
-        try {
-            if (Path.of(systemPropertyName + ".tmp").toFile().exists()) {
-                final String string = Files.readString(Path.of(systemPropertyName + ".tmp"));
-                final String[] split = string.split(";", 5);
-                final int x = Integer.parseInt(split[0]);
-                final int y = Integer.parseInt(split[1]);
-                final int w = Integer.parseInt(split[2]);
-                final int h = Integer.parseInt(split[3]);
-                final String data = split[2];
+        String string = getLocalStorage().getString(systemPropertyName);
+        if (string != null) {
+            string = string.replace("\\n", "\n");
+            final String[] split = string.split(";", 5);
+            final int x = Integer.parseInt(split[0]);
+            final int y = Integer.parseInt(split[1]);
+            final int w = Integer.parseInt(split[2]);
+            final int h = Integer.parseInt(split[3]);
+            final String data = split[2];
 
-                setXY(x, y);
+            setXY(x, y);
 
-                bg.setSize(w, h);
-                text.setBounds(w, h);
-                touchButton.setSize(w, h);
-                text.setText(data);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            bg.setSize(w, h);
+            text.setBounds(w, h);
+            touchButton.setSize(w, h);
+            text.setText(data);
         }
     }
 
@@ -230,7 +235,7 @@ public class DebugPanel extends DisplayObjectContainer {
 
         DebugPanel.setEnabled(true);
 
-        for(int i = 0; i < 1000; i ++) {
+        for (int i = 0; i < 1000; i++) {
             DebugPanel.show("debug-panel-" + i).ifPresent(debugPanel -> {
                 debugPanel.setText(debugPanel.getX());
                 debugPanel.addEventListener(Event.EACH_FRAME, event -> {
@@ -239,9 +244,13 @@ public class DebugPanel extends DisplayObjectContainer {
             });
         }
 
+        System.out.println(ApplicationMainClassNameExtractor.get());
+
+
         D2D2.loop();
         DebugPanel.saveAll();
     }
+
 
 }
 
